12a0317e124b4a9c603ad4cf4e3356323aa17a3c
Vladislav0101 <by.vlad01@mail.ru>
2019-09-25 00:20:27 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function zeros(expression) {
   let zero = 0;
   while (expression > 0){
-    if (expression % 10 == 0) 
-    zero++;
+    if ((expression % 10) = 0)zero++;
+    expression / 10;
   }
   return zero;
 
   //let zero;
-  //for( ; expression)
+  //for( ; true; )
 }
