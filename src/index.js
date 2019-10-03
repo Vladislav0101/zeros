4b6528400410229d7b8aa0b129d987ba3a840811
Vladislav0101 <by.vlad01@mail.ru>
2019-10-03 17:42:51 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 module.exports = function zeros(expression) {
-  let zero = 0;
-  while (expression > 0){
-    if ((expression % 10) = 0)zero++;
-    expression / 10;
-  }
-  return zero;
 
-  //let zero;
-  //for( ; true; )
+
+
+let a = expression.split("*");
+let nul = 0
+for (let ind = 0; ind <= a; ind++){
+  let num = parseInt(a[ind])
+
+let mul = 1;
+for (let i = 1; i <= num; i++){
+mul *= i; 
+}
+let z;
+let nul = 0;
+let b;
+for (z = 1; z <= mul; z++){
+  if ((z % 5 == 0) & (z % 2 == 0)){
+    mul = mul/10;
+    nul++;
+  }
+}
+}
+return nul;
+
+// let a = expression.split("*");
+// let num = parseInt(a[0]);
+// let mul = 1;
+// for (let i = 1; i <= num; i++){
+// mul *= i; 
+// }
+// let z;
+// let nul = 0;
+// let b;
+// for (z = 1; z <= mul; z++){
+//   if ((z % 5 == 0) & (z % 2 == 0)){
+//     mul = mul/10;
+//     nul++;
+//   }
+// }
+// }
+// return nul;
 }
